190ccad50c8c3ace6d919b96477df1f412f2bd59
xraybrain <xraybrain@gmail.com>
2018-07-13 06:09:23 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io(); // this is used to listen for data from the server and to send data to the server
 
+
+function scrollToBottom() {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -21,6 +39,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -38,6 +57,8 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a  = jQuery('<a target="_blank">My current location</a>');
     // li.text(`${message.from} ${formattedTime}: `);
